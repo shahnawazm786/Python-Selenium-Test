b7349f32052bfa32c0c712079e7bd0649a5155d0
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-01-24 13:40:33 +0530

    python selenium

--- Practice/VariableExample.py
+++ Practice/VariableExample.py
@@ -6,3 +6,6 @@ Y = 100.55
 print(Y)
 Z = 200.8999
 print(Z)
+
+name = 'Python Example'
+print(name)
--- SeleniumTest.py
+++ SeleniumTest.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(executable_path="Drivers/chromedriver.exe")
-driver.set_page_load_timeout(10)
+driver.set_page_load_timeout(5)
 driver.get("https://www.google.com")
 driver.maximize_window()
-driver.find_element_by_name("q").send_keys("Automation Framework")
-driver.find_element_by_name("q").send_keys(Keys.ENTER)
-driver.close()
+#driver.find_element_by_name("q").send_keys("Automation Framework")
+#driver.find_element_by_name("q").send_keys(Keys.ENTER)
+searchBox = driver.find_element(By.XPATH, "//input[@name='q']")
+searchBox.sendkeys("selenium")
+searchBox.sendkeys(Keys.Enter)
+
+#driver.close()
