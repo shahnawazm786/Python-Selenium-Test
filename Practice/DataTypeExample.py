65c5020262b375436d295edbb21dd84eccbcac30
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-02-02 10:50:01 +0530

    data example

--- Practice/DataTypeExample.py
+++ Practice/DataTypeExample.py
@@ -1,3 +1,9 @@
 a = [10, 20, 30, 40, 50]
 print("Example of List")
 print(a)
+print("Dictionary example")
+employee = {'Rollno': 1, 'Name':"Mohammad"}
+print(employee)
+print("Set example")
+s = (100, 200, 300, 500)
+print(s)
