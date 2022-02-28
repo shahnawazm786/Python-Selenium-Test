0b6616ee49abdd546cd9be9eab6543d998fe8db4
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-02-28 21:25:50 +0530

    deliveries

--- Practice/IPL_Deliveries.py
+++ Practice/IPL_Deliveries.py
@@ -5,3 +5,12 @@ df1=pd.DataFrame(deli)
 
 # Column of dataset
 print(df1.columns)
+
+print(df1.head(5))
+
+# Show the fielder who dismiss the batsman
+print(df1['fielder'].head(10))
+
+# show the dismissal_kind
+
+print(df1['dismissal_kind'])
