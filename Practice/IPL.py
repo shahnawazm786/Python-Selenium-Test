ce97a3d5a50030c643e927aa308295d23526c668
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-02-28 18:29:48 +0530

    ipl

--- Practice/IPL.py
+++ Practice/IPL.py
@@ -1,4 +1,6 @@
 import pandas as pd
-ipl = pd.read_csv('Drivers/matches.csv')
+ipl = pd.read_csv('F:\\PythonProject\\Drivers\\matches.csv')
 df = pd.DataFrame(ipl)
 print(df.head())
+print(df.columns)
+print(df[['team1', 'team2']])
