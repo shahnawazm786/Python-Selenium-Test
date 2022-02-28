d15bbdbc909d2f45313a35167898006466f1934b
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-02-28 18:38:08 +0530

    ipl

--- Practice/IPL.py
+++ Practice/IPL.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 ipl = pd.read_csv('F:\\PythonProject\\Drivers\\matches.csv')
 df = pd.DataFrame(ipl)
 print(df.head())
@@ -6,3 +7,7 @@ print(df.columns)
 print(df[['team1', 'team2']])
 print(df[['team1', 'team2', 'winner']])
 print(df['winner'].value_counts())
+
+df['winner'].value_counts(normalize=True).plot(kind='bar')
+plt.show()
+
