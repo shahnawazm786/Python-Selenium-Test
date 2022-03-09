425987ba25278008fae4baf5ef3da7803986a45d
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-03-09 22:00:33 +0530

    if_else

---
+++ Practice/if_else.py
@@ -0,0 +1,7 @@
+if(100+200):
+    print('My name is khan')
+else:
+    print("I am yogi")
+
+    ## string -> Ascii convert -> Number -> Positive
+    ## 0 -> Machine Language - > (1,0) -> True, False
